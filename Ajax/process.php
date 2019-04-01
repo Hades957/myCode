9d6cb574efe1586a0a6e7b847b5efeb395e4e912
Bifeng Zheng <z1464137312@163.com>
2019-04-02 04:00:22 +0800

    Js原生Ajax学习
    涉及form表单get和post提交、Ajax异步数据处理以及对php数据、数据库中数据进行存储和获取

---
+++ Ajax/process.php
@@ -0,0 +1,27 @@
+<?php 
+	// echo "Hello World!";
+	header("content-type:text/html;charset=utf-8");
+	if(isset($_GET['name'])){
+		echo "GET: 你的名字是".$_GET['name'];
+
+	}
+
+	# 连接数据库
+	$conn = mysqli_connect("localhost","root",'root','ajaxtest');
+
+	if(isset($_POST['name'])){
+		// echo "POST: 你的名字是".$_POST['name'];
+
+		#将拿到的数据转化一下
+		$name = mysqli_real_escape_string($conn,$_POST['name']);
+
+		$query = "INSERT INTO users(name) VALUES('$name')";
+		mysqli_query($conn,"set name uft8");
+		if(mysqli_query($conn,$query)){
+			echo '用户添加成功';
+		}else{
+			echo '用户添加失败'.mysqli_error($conn);
+		}
+
+	}
+ ?>
